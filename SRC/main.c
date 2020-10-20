200523fa48b4e8737afeba264be8de49871a6a9b
surendra <surendra.bitwerks@gmail.com>
2020-10-20 15:44:28 +0545

    First Test of GIT for Keil.

---
+++ SRC/main.c
@@ -0,0 +1,9 @@
+#include "LPC17xx.h"                    // Device header
+
+
+int
+main(void)
+{
+  return 0;
+} 
+
---
+++ UV5/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,27 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'GUI_TEST' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "LPC17xx.h"
+
+/* Keil::CMSIS Driver:I2C:2.5.0 */
+#define RTE_Drivers_I2C0                /* Driver I2C0 */
+        #define RTE_Drivers_I2C1                /* Driver I2C1 */
+        #define RTE_Drivers_I2C2                /* Driver I2C2 */
+/* Keil::Device:Startup:1.0.0 */
+#define RTE_DEVICE_STARTUP_LPC17XX      /* Device Startup for NXP17XX */
+
+
+#endif /* RTE_COMPONENTS_H */
